b8ed25991b476035452763c9ae045727aae57826
jankikansagra03 <122074631+jankikansagra03@users.noreply.github.com>
2024-04-22 14:07:23 +0530

    Update pay.php

--- pay.php
+++ pay.php
@@ -4,8 +4,8 @@ require_once 'vendor/autoload.php'; // Include the Razorpay library
 
 use Razorpay\Api\Api;
 
-$api_key = 'rzp_test_iD5aLBBZHZuTq6';
-$api_secret = 'QallnX5ljwZaYK97dSZ5XjSs';
+$api_key = '';
+$api_secret = '';
 $total=$_SESSION['total'];
 //$total=$_REQUEST['total'];
 $api = new Api($api_key, $api_secret);
